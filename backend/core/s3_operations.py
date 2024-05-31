73bb6ebff61900946c172f76fbbb276ec3bdff7b
wantedbear007 <pratapsinghbhanu444@gmail.com>
2024-05-31 14:50:26 +0530

    added upload and download logic.

--- backend/core/s3_operations.py
+++ backend/core/s3_operations.py
@@ -1,25 +1,37 @@
 import boto3
 from botocore.exceptions import ClientError
+import uuid
 
-# debug
-import logging
+# user defined
+from s3_responses import S3Response
+
 
 # to upload file
-def upload_file(file_name: str, bucket: str):
+def upload_file(file_name: str, bucket: str) -> S3Response:
+    object_name: str = str(uuid.uuid1())
     
-    print("inside function")
-    
-    object_name: str = file_name
-    
-    s3_client = boto3.client('s3')
     try:
+        s3_client = boto3.client('s3')
         response = s3_client.upload_file(file_name, bucket, object_name)
-        print(response)
-    
+        return S3Response(status=False, desc=object_name)
+        
+        
     except ClientError as e:
-        print(e)
-        logging.error(e)
+        return S3Response(status=False, desc=str(e))
         
 
 
-upload_file("bhanupratapCV.pdf", "prataptech-guardian")
+# to download file 
+def download_file(file_name: str, bucket: str, object_name: str):
+    try:
+        s3_client = boto3.client("s3")
+        response = s3_client.download_file(bucket, object_name, object_name)
+        print(response)
+    except Exception as e:
+        print(e)
+        
+
+# upload_file("bhanupratapCV.pdf", "prataptech-guardian")
+# 6557885e-1f24-11ef-848b-8298d9ac4759
+
+# download_file("6557885e-1f24-11ef-848b-8298d9ac4759", "prataptech-guardian", "6557885e-1f24-11ef-848b-8298d9ac4759")
---
+++ backend/core/s3_responses.py
@@ -0,0 +1,6 @@
+
+
+class S3Response:
+    def __init__(self, status: bool, desc: str):
+        self.statusCode = statusCode,
+        self.desc = desc
