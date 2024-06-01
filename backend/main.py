109b5fd41c23aaac77f0cbeb412ce7515b983f44
wantedbear007 <pratapsinghbhanu444@gmail.com>
2024-06-01 17:26:27 +0530

    frontend changes

--- backend/main.py
+++ backend/main.py
@@ -17,18 +17,13 @@ app = FastAPI()
 
 
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1:8000/v1/upload",
-    "http://localhost:8000",
-    "http://localhost:5173",
-    "https://guardian-assist.vercel.app/"
+ "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    # allow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
